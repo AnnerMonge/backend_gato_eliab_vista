a6d9f058ffe81eb64ff63ffc1da8a275ab5e362b
AnnerMonge <jadircruz750@gmail.com>
2026-08-11 12:19:50 -0600

    actualuzacion  firebase

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp, cert, getApps } from firebase-admin/app;
-import { getFirestore } from firebase-admin/firestore;
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 const serviceAccount = {
 projectId: process.env.FIREBASE_PROJECT_ID,
 clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
